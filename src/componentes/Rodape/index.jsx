d912eacb42c89f6f932ab0b5103f3748ba71c4d4
Diego Fernando <diego_maximo160@hotmail.com>
2024-03-27 18:42:00 -0300

    link para home no rodape

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -1,9 +1,10 @@
 import estilos from './rodape.module.css'
+import Link from 'next/link'
 
 export default function Rodape() {
     return(
         <div className={estilos.container}>
-            <p>© 2022 Ótica Vida. - Todos os direitos reservados.</p>
+            <p>© 2022 <Link href="/" className={estilos.link}>Ótica Vida.</Link> - Todos os direitos reservados.</p>
         </div>
     )
 }
